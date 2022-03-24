108535ea8f9325bbf4ffb95e8f8457d4b411b2d3
Jules Pognante <pognante.jules@gmail.com>
2022-03-24 18:48:34 +0100

    completion de la nouvelle page permettant d'afficher le prix d'un jeu sur tous les magasins le vendant, avec un lien pour l'acheter

--- jeu.js
+++ jeu.js
@@ -7,13 +7,37 @@ function affiche(id){
 
 window.onload = function(){
   if(localStorage.getItem('jeuSelect')){
-    affichage(localStorage.getItem('jeuSelect'));
+    fetchDatas(localStorage.getItem('jeuSelect'));
   }else{
     console.log('Pas de jeu sélectionné')
   }
+  if(!localStorage.getItem('storesNames')){
+    fetch("https://www.cheapshark.com/api/1.0/stores")
+    .then(manageErrors)
+    .then(function(res){
+      storesArrayToObject(res);
+    })
+  }
+ 
 }
 
-function affichage(id){
+function storesArrayToObject(res){
+  let storesObject = {};
+  let storesImage = {};
+  
+  for(obj of res){
+    let storeID = obj['storeID'];
+    let storeName = obj['storeName'];
+    let storeImage = "https://www.cheapshark.com"+obj['images']['icon'];
+    storesObject[storeID] = storeName;
+    storesImage[storeID] = storeImage;
+  }
+  localStorage.setItem('storesNames',JSON.stringify(storesObject));
+  localStorage.setItem('storesImg',JSON.stringify(storesImage));
+  
+}
+
+function fetchDatas(id){
   fetch("https://www.cheapshark.com/api/1.0/games?id="+localStorage.getItem('jeuSelect'))
   .then(manageErrors)
   .then(function(res){
@@ -29,9 +53,37 @@ function manageErrors(response){
 }
 
 function affichageJeu(json){
+  let storesList = JSON.parse(localStorage.getItem('storesNames'));
+  let storesImg = JSON.parse(localStorage.getItem('storesImg'));
+  console.log(storesImg);
+  console.log(localStorage.getItem('jeuSelect'));
   let infos = json['info'];
+  let deals = json['deals'];
+  let list = document.createElement('ul');
+  let imgJeu = document.createElement('img');
+  imgJeu.src = infos['thumb'];
+  for(deal of deals){
+    let listItem = document.createElement('li');
+    let img = document.createElement('img');
+    img.src = storesImg[deal['storeID']];
+    listItem.appendChild(img);
+    listItem.innerHTML += storesList[deal['storeID']];
+    
+    
+    if(deal['price'] == deal['retailPrice']){
+      listItem.innerHTML += " -- Prix : " + deal['retailPrice'];
+    } else{
+      listItem.innerHTML += " -- Prix : <strike>" + deal['retailPrice'] + "</strike> => " + deal['price'] + " (-" + Math.round(deal['savings']) + "%)";
+    }
+    listItem.innerHTML += "<a href=https://www.cheapshark.com/redirect?dealID=" + deal['dealID'] + "> Acheter </a>";
+    listItem.className = "magasin";
+    list.appendChild(listItem);
+  }
+
   let parNom = document.createElement('p');
   parNom.innerText = infos['title'];
+  information.appendChild(imgJeu);
   information.appendChild(parNom);
+  information.appendChild(list);
   console.log(json['info'])
 }
