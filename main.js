a08fe1e1843591af90942caea1e37b4aa4e4aaff
Jules Pognante <pognante.jules@gmail.com>
2022-03-24 10:05:42 +0100

    Ajout de texte et d'un lien sur les jeux lorsqu'ils s'affichent

--- main.js
+++ main.js
@@ -8,15 +8,26 @@
     })
 } */
 
+
+
+var doc = document.getElementById('bloc-resultats');
+
 window.onload = function(){
-    fetch('https://www.cheapshark.com/api/1.0/deals?onSale=1&sortBy="recent"&pageSize=10')
-    .then(function(res){
-        return res.json();
-    })
+    fetch('https://www.cheapshark.com/api/1.0/deals?onSale=1&sortBy=%22recent%22&pageSize=10')
+    .then(manageErrors)
     .then(function(json){
-        console.log(json);
         afficheJeux(json);
     })
+    .catch(function(error){
+        noResult();
+    })
+}
+
+function manageErrors(response){
+    if(!response.ok){
+        throw Error(response.statusText);
+    }
+    return response.json();
 }
 
 function afficheJeux(json){
@@ -27,18 +38,29 @@ function afficheJeux(json){
         imgJeu.src = elem['thumb'];
         let titre = document.createElement('p');
         titre.innerText = elem['title'] + " - ";
-        let price = document.createElement('span');
-        price.innerText=elem['normalPrice'];
-        price.className = 'oldPrice'
-        let reducePrice = document.createElement('p');
-        reducePrice.innerText = elem['salePrice']
+        titre.innerHTML += '<span class="oldPrice">'+elem['normalPrice']+'</span> => '+elem['salePrice']+' (-'+Math.round(elem['savings'])+'%)';
+        let notes = document.createElement('p');
+        if(elem['metacriticScore']!=0 && elem['steamRatingText']!=null){
+            notes.innerText = "Metacritic : " + elem['metacriticScore'] + ' -- Score Steam : ' + elem['steamRatingText'];
+        }
+        else{
+            notes.innerHTML = "Aucune notes renseignées"
+        }
+        let link = document.createElement('a');
+        link.href = "https://www.cheapshark.com/redirect?dealID="+elem['dealID'];
+        link.innerText = "Acheter";
         bloc.appendChild(imgJeu);
         bloc.appendChild(titre);
-        bloc.appendChild(price);
-        bloc.appendChild(reducePrice);
-        document.getElementById('bloc-resultats').appendChild(bloc);
-        console.log(bloc.classList);
+        bloc.appendChild(notes);
+        bloc.appendChild(link);
+        doc.appendChild(bloc);
     }
     
     
+}
+
+function noResult(){
+    let noResultP = document.createElement('p');
+    noResultP.innerHTML = '<p class="info-vide">( &empty; Aucun machin trouvé )</p>'
+    doc.appendChild(noResultP);
 }
